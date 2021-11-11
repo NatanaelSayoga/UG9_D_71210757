4d7b617f048ca783a1c1b99ea0e34959fb5ae274
Yoga <natanael.sayoga@ti.ukdw.ac.id>
2021-11-11 21:53:30 +0700

    unggahan ketiga

---
+++ 2_D_71210757.py
@@ -0,0 +1,5 @@
+u1 = int(input("Uang saku anak pertama :"))
+u2 = int(input("Uang saku anak kedua :"))
+u3 = int(input("Uang saku anak ketiga :"))
+r = (u1 + u2 + u3) / 3
+print("Rata-rata dari " + str(u1) + "+" + str(u2) + "+" + str(u3) + " adalah " + str(r))

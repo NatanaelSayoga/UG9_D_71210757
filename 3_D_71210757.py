23e07805e498f385527b87b6a7e8e2f941a25df6
Yoga <natanael.sayoga@ti.ukdw.ac.id>
2021-11-11 21:53:56 +0700

    unggahan keempat

---
+++ 3_D_71210757.py
@@ -0,0 +1,6 @@
+lawas_1 = input("Masukkan daftar belanja 1: ")
+lawas_2 = input("Masukkan daftar belanja 2: ")
+anyar_1 = input("Tambahkan data ke daftar belanja 1: ")
+anyar_2 = input("Tambahkan data ke daftar belanja 2: ")
+print("Daftar belanja 1 adalah " + lawas_1 + ", " + anyar_1)
+print("Daftar belanja 2 adalah " + lawas_2 + ", " + anyar_2)
